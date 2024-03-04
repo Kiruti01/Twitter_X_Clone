86039f5627a6e07fe8c8bdc1b3eec1d2ae447b89
Michael Kiruti <mikekirutic@gmail.com>
2024-03-04 16:39:35 +0300

    1.15.32

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -3,6 +3,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog";
 // import { X } from "lucide-react";
 // import { cn } from "@/lib/utils";
 import { title } from "process";
+import { X } from "lucide-react";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -11,8 +12,6 @@ interface ModalProps {
   footer?: ReactElement;
   step?: number;
   totalSteps?: number;
-  isEditing?: boolean;
-  title: string;
 }
 
 export default function Modal({
@@ -22,16 +21,22 @@ export default function Modal({
   onClose,
   step,
   totalSteps,
-  isEditing,
 }: ModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-black">
-        <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-        </DialogHeader>
+      <DialogContent className="bg-black p-1">
+        <div className="flex items-center gap-6">
+          <button className="p-1 border-0 text-white hover:opacity-70 transition w-fit">
+            <X size={28} onClick={onClose} />
+          </button>
+          {step && totalSteps && (
+            <div className="text-xl font-bold">
+              Step {step} of {totalSteps}
+            </div>
+          )}
+        </div>
         <div className="mt-4">{body}</div>
-        {footer && <div className="mt-4">{footer}</div>}
+        {footer && <div>{footer}</div>}
       </DialogContent>
     </Dialog>
   );
